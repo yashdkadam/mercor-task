5c716cbc63dcb189616ebf252d74801b2d678ceb
yashdkadam <kadamdyash55@gmail.com>
2023-05-21 15:23:29 +0530

    initial commit

---
+++ src/pages/Commercial.js
@@ -0,0 +1,139 @@
+import "./Commercial.css";
+const Commercial = () => {
+  return (
+    <div className="commercial">
+      <img
+        className="mdivideo-outline-icon"
+        alt=""
+        src="/mdivideooutline.svg"
+      />
+      <img className="mditext-box-plus-icon" alt="" src="/mditextboxplus.svg" />
+      <img className="bxsfile-icon" alt="" src="/bxsfile.svg" />
+      <div className="untitled-document">Untitled document</div>
+      <div className="all-actions">
+        <div className="file">File</div>
+        <div className="edit">Edit</div>
+        <div className="view">View</div>
+        <div className="tools">Tools</div>
+        <div className="insert">Insert</div>
+        <div className="extensions">Extensions</div>
+        <div className="document">document</div>
+        <div className="help">Help</div>
+      </div>
+      <img className="picture-icon" alt="" src="/picture.svg" />
+      <img
+        className="simple-line-iconscalender"
+        alt=""
+        src="/simplelineiconscalender.svg"
+      />
+      <div className="share-lock">
+        <div className="share-lock-child" />
+        <img
+          className="material-symbolslock-outline-icon"
+          alt=""
+          src="/materialsymbolslockoutline.svg"
+        />
+        <div className="share">Share</div>
+      </div>
+      <div className="commercial-child" />
+      <img
+        className="system-uiconsbackward"
+        alt=""
+        src="/systemuiconsbackward.svg"
+      />
+      <img
+        className="system-uiconsforward"
+        alt=""
+        src="/systemuiconsforward.svg"
+      />
+      <img
+        className="flat-color-iconsprint"
+        alt=""
+        src="/flatcoloriconsprint.svg"
+      />
+      <img
+        className="icon-park-outlinecorrect"
+        alt=""
+        src="/iconparkoutlinecorrect.svg"
+      />
+      <img className="vector-icon" alt="" src="/vector.svg" />
+      <div className="div">100%</div>
+      <img className="vector-icon1" alt="" src="/vector1.svg" />
+      <img className="vector-icon2" alt="" src="/vector2.svg" />
+      <img className="vector-icon3" alt="" src="/vector3.svg" />
+      <img className="vector-icon4" alt="" src="/vector4.svg" />
+      <div className="normal-text">Normal Text</div>
+      <div className="commercial-item" />
+      <div className="commercial-inner" />
+      <div className="commercial-child1" />
+      <div className="commercial-child2" />
+      <div className="commercial-child3" />
+      <div className="commercial-child4" />
+      <div className="commercial-child5" />
+      <img className="plus-icon" alt="" src="/plus.svg" />
+      <div className="open">{`Open... `}</div>
+      <div className="commercial-child6" />
+      <div className="div1">11</div>
+      <div className="uilitalic" />
+      <img className="vector-icon5" alt="" src="/vector5.svg" />
+      <img className="octiconbold-24" alt="" src="/octiconbold24.svg" />
+      <img
+        className="clarityunderline-line-icon"
+        alt=""
+        src="/clarityunderlineline.svg"
+      />
+      <img
+        className="fluenttext-color-20-filled-icon"
+        alt=""
+        src="/fluenttextcolor20filled.svg"
+      />
+      <img
+        className="material-symbolsformat-color-icon"
+        alt=""
+        src="/materialsymbolsformatcolorfillrounded.svg"
+      />
+      <img
+        className="svg-spinners3-dots-move-icon"
+        alt=""
+        src="/svgspinners3dotsmove.svg"
+      />
+      <img
+        className="material-symbolsbrush-icon"
+        alt=""
+        src="/materialsymbolsbrush.svg"
+      />
+      <img className="uilarrow-icon" alt="" src="/uilarrow.svg" />
+      <div className="commercial-child7" />
+      <i className="type-to">Type @ to insert</i>
+      <div className="commercial-child8" />
+      <img className="phplus-bold-icon" alt="" src="/phplusbold.svg" />
+      <img
+        className="logosgoogle-maps-icon"
+        alt=""
+        src="/logosgooglemaps.svg"
+      />
+      <img className="group-icon2" alt="" src="/group2.svg" />
+      <img className="group-icon3" alt="" src="/group3.svg" />
+      <img className="notolight-bulb-icon" alt="" src="/notolightbulb.svg" />
+      <img
+        className="material-symbolsstar-outline-icon"
+        alt=""
+        src="/materialsymbolsstaroutline.svg"
+      />
+      <img className="tablerpaint-icon" alt="" src="/tablerpaint.svg" />
+      <img
+        className="yash-wanted-design-1-icon"
+        alt=""
+        src="/yash-wanted-design-1@2x.png"
+      />
+      <div className="macbook-air-1" />
+      <img className="icon" alt="" src="/9@2x.png" />
+      <img className="icon1" alt="" src="/8@2x.png" />
+      <img className="icon2" alt="" src="/7@2x.png" />
+      <div className="commercial-child9" />
+      <img className="vector-icon6" alt="" src="/vector6.svg" />
+    </div>
+  );
+};
+
+export default Commercial;
